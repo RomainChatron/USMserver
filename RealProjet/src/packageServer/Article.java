c730ddf207d536127ad597978b0e52cad2e87690
remiv <remiv@192.168.1.30>
2016-05-03 05:16:43 +0200

    add Article.java

---
+++ RealProjet/src/packageServer/Article.java
@@ -0,0 +1,51 @@
+package packageServer;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+import vInterface._Article;
+
+
+@SuppressWarnings("serial")
+public class Article extends UnicastRemoteObject implements _Article {
+	
+	private int idA;
+	private String dateA;
+	private int idDi;
+	
+	public Article() throws RemoteException{
+		// int idA = DiaryDB.getMaxDiary() + 1;
+		setIdA(idA);
+		setDateA("");
+	}
+	
+	public Article(int idA, String date, int idDi) throws RemoteException {
+		setDateA(date);
+		setDateA(date);
+		setIdDi(idDi);
+	}
+
+	public String getDateA() throws RemoteException{
+		return dateA;
+	}
+
+	public void setDateA(String dateA) throws RemoteException{
+		this.dateA = dateA;
+	}
+
+	public int getIdDi() throws RemoteException{
+		return idDi;
+	}
+
+	public void setIdDi(int articleDiary) throws RemoteException{
+		this.idDi = articleDiary;
+	}
+
+	public int getIdA() throws RemoteException{
+		return idA;
+	}
+
+	public void setIdA(int idA) throws RemoteException{
+		this.idA = idA;
+	}
+}
